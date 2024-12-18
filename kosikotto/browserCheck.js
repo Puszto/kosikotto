ce08d81e79270910308c5c571b67c533fd40fd3c
Puszto <ifj.kosik.otto@gmail.com>
2024-12-18 16:16:43 +0100

    Delete kosikotto directory

--- kosikotto/browserCheck.js
+++
@@ -1,7 +0,0 @@
-const isFirefox = navigator.userAgent.toLowerCase().includes('firefox');
-
-if (isFirefox) {
-  document.documentElement.classList.add("firefoxBrowser");
-} else {
-  document.documentElement.classList.add("nonFirefoxBrowser");
-}
